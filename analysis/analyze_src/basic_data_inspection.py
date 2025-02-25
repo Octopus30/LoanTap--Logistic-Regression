d3a9e2ff7fd8ee63796c257ffb9899a2d0a63b40
Akhil Reddy <akhil.reddy@TimesofIndia.com>
2025-02-25 18:20:43 +0530

    EDA in progress

--- analysis/analyze_src/basic_data_inspection.py
+++ analysis/analyze_src/basic_data_inspection.py
@@ -38,10 +38,6 @@ class DatatypesInspectionStrategy(DataInspectionStrategy):
         """
         print("Datatypes of the Nullcounts in the dataframe: ")
         print(data.info())
-        print("Summary statistics of Numerical Columns:  ")
-        print(data.descripbe())
-        print("Summary statistics of Categorical Columns: ")
-        print(data.describe(include =['O']))
 
 #Concrete strategy for summary inspection
 #------------------------------------------
@@ -60,10 +56,20 @@ class SummaryInspectionStrategy(DataInspectionStrategy):
 
         """
         print("Summary statistics of Numerical Columns:  ")
-        print(data.descripbe())
+        print(data.describe())
         print("Summary statistics of Categorical Columns: ")
         print(data.describe(include =['O']))
 
+
+# #Concrete strategy for finding the duplicate values
+# #----------------------------------------------------------------
+# class DuplicateInspectionStrategy(DataInspectionStrategy):
+#     def inspect(self, data: pd.DataFrame):
+#         """
+#         Inspect the duplicate values in the dataframe"""
+        
+#         print("Duplicate values in the dataframe: ")
+#         print(data.duplicated().sum())
 
 #context class that uses  DataInspectionStrategy
 #------------------------------------------------
@@ -79,5 +85,21 @@ class DataInspection:
     def execute_inspection(self, data: pd.DataFrame):
         #executes the inspection strategy
         self._strategy.inspect(data)
+
+if __name__ == "__main__":
+
+    # df = pd.read_csv('extracted_data/logistic_regression.csv')
+
+    # # inspector = DataInspection(DatatypesInspectionStrategy())
+
+    # # print(inspector.execute_inspection(df))
+
+    # # inspector_summary = DataInspection(SummaryInspectionStrategy())
+
+    # # print(inspector_summary.execute_inspection(df))
+
+    # inspector_duplicates = DataInspection(DuplicateInspectionStrategy())
+    # print(inspector_duplicates.execute_inspection(df))
+    pass
 
 
